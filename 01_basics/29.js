70e49b58d4e7eb4160671d4084bc43025d5b75df
Radhikacrypto <114675656+Radhikacrypto@users.noreply.github.com>
2024-03-05 17:55:20 +0000

    loop config done

--- 01_basics/29.js
+++ 01_basics/29.js
@@ -2,7 +2,7 @@
 
 // ["", "", ""];//araay of strings
 // [{}, {}, {}, ]//array of objects 
-
+ 
 const arr =[1, 2, 3, 4, 5]
 for (const num of arr) {
     console.log(num);
@@ -11,6 +11,7 @@ const greetings ="hello world"
 for (const greet of greetings) {
     console.log(`each char is ${greet}`)
 }
+
 
 //maps
 
@@ -24,3 +25,82 @@ console.log(map);
 for (const [key, value] of map) {
     console.log(key);
 }
+
+const myObj={
+    //two ways of writing ..although both will throw error
+    game1: 'NFS',//'game': 'NFS'
+    game2: 'spiderman'//'game2': 'spiderman'
+}
+//this will throw error as this will not iterate objects ..objects are not iterable ..there are different methods of doing so 
+
+// for(const [key, value] of myObj) {
+//     console.log(key, ':-', value);
+// }
+
+const myobj={
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: "swift by apple"
+
+}
+
+for (const key in myobj) {
+   console.log(`${key} shortcut is for ${myobj[key]}`);
+}
+
+const programming=["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(key);//output is coming so coz ...array default keys is 0, 1 , 2,...
+    console.log(programming[key]);//notice the difference 
+}
+
+const coding=["js", "ruby", "cpp", "java", "py"]
+
+coding.forEach( function (item) {
+    console.log(item);
+} )
+ 
+//using arrow functiom 
+coding.forEach( (item) => {
+    console.log("using arrow function",item);
+})
+
+//another method
+
+function printMe(item) {
+    console.log("printing using printme user made function", item);
+}
+
+coding.forEach(printMe)
+
+coding.forEach( (item, index, arr) => {
+ console.log(item , index, arr);//in sabh cheezo ko acces hota hai for each ke pass isliye most used hai 
+})
+// ek array ke andar bahut sarre objects hai 
+const mc =[
+   {//yeh objects hai 
+    languagename: "javascript",
+    languageFilename: "js"
+   },
+
+   {
+    languagename: "c++",
+    languageFilename: "cpp"
+   },
+
+   {
+    languagename: "java",
+    languageFilename: "java"
+   },
+
+   {
+    languagename: "python",
+    languageFilename: "py"
+   }
+]
+
+mc.forEach( (item) => {
+    console.log(item.languagename);
+})
